7e42932668466c50c911b9ba0e665825ac801d1a
MilchakovEgor <113169893+MilchakovEgor@users.noreply.github.com>
2023-02-08 20:24:19 +0300

    Update Program.cs

--- praka4/Program.cs
+++ praka4/Program.cs
@@ -1,8 +1,10 @@
-﻿using Everydayinik;
+using Ejednevnik;
 
-bool isListenning = true;
+
+bool Bool = true;
 
 DateTime now = DateTime.Now;
+
 
 int plusDay = 0;
 List<Date1> zametki = new List<Date1>() { };
@@ -13,11 +15,15 @@ int posicion = 1;
 
 Date1 zametka1 = new Date1();
 
+
 zametka1.Name = "Сходить в МПТ";
-zametka1.Description = "Cходить и посидеть на всех парах";
+zametka1.Description = "Cходить и поспать на парах";
+zametka1.Description = "...";
 zametka1.Done = "08.02.23";
 zametka1.Dade = new DateTime(2023, 02, 08).ToLongDateString();
 zametki.Add(zametka1);
+
+
 
 Date1 zametka2 = new Date1();
 zametka2.Name = "Покушать";
@@ -26,6 +32,8 @@ zametka2.Done = "09.02.23";
 zametka2.Dade = new DateTime(2023, 02, 09).ToLongDateString();
 zametki.Add(zametka2);
 
+
+
 Date1 zametka3 = new Date1();
 zametka3.Name = "Отдохнуть";
 zametka3.Description = "Я устал";
@@ -33,12 +41,17 @@ zametka3.Done = "11.02.23";
 zametka3.Dade = new DateTime(2023, 02, 11).ToLongDateString();
 zametki.Add(zametka3);
 
+
+
 Date1 zametka4 = new Date1();
 zametka4.Name = "Сходить в магазин";
-zametka4.Description = "Купить хлеб";
+zametka4.Description = "Купить хлеб и корнишоны";
 zametka4.Done = "15.02.23";
 zametka4.Dade = new DateTime(2023, 02, 15).ToLongDateString();
 zametki.Add(zametka4);
+
+
+
 
 Date1 zametka5 = new Date1();
 zametka5.Name = "Доделатьвсе праки";
@@ -47,7 +60,9 @@ zametka5.Done = "07.02.23";
 zametka5.Dade = new DateTime(2023, 02, 07).ToLongDateString();
 zametki.Add(zametka5);
 
-void zamet(ConsoleKeyInfo key)
+
+
+void zametka(ConsoleKeyInfo key)
 {
     if (zametka1.Dade == now.AddDays(plusDay).ToLongDateString())
     {
@@ -59,13 +74,13 @@ void zamet(ConsoleKeyInfo key)
         {
             Console.Clear();
             Console.SetCursorPosition(2, 1);
-            Console.WriteLine("1:Дата создания " + zametki[0].Dade);
+            Console.WriteLine("1:Дата" + zametki[0].Dade);
             Console.SetCursorPosition(2, 2);
-            Console.WriteLine("2:Название " + zametki[0].Name);
+            Console.WriteLine("2:Название" + zametki[0].Name);
             Console.SetCursorPosition(2, 3);
-            Console.WriteLine("3:Описание " + zametki[0].Description);
+            Console.WriteLine("3:Описание" + zametki[0].Description);
             Console.SetCursorPosition(2, 4);
-            Console.WriteLine("4:Дата окрнчания " + zametki[0].Done);
+            Console.WriteLine("4:Дата окрнчания" + zametki[0].Done);
             Console.SetCursorPosition(2, 5);
             Console.WriteLine("Чтобы выйти обратно нажмите любую кнопку");
         }
@@ -73,7 +88,7 @@ void zamet(ConsoleKeyInfo key)
         {
             Console.Clear();
             Console.SetCursorPosition(2, 1);
-            Console.WriteLine("1:Дата создания " + zametki[1].Dade);
+            Console.WriteLine("1:Дата " + zametki[1].Dade);
             Console.SetCursorPosition(2, 2);
             Console.WriteLine("2:Название " + zametki[1].Name);
             Console.SetCursorPosition(2, 3);
@@ -94,7 +109,7 @@ void zamet(ConsoleKeyInfo key)
         {
             Console.Clear();
             Console.SetCursorPosition(2, 1);
-            Console.WriteLine("1:Дата создания " + zametki[2].Dade);
+            Console.WriteLine("1:Дата " + zametki[2].Dade);
             Console.SetCursorPosition(2, 2);
             Console.WriteLine("2:Название " + zametki[2].Name);
             Console.SetCursorPosition(2, 3);
@@ -108,7 +123,7 @@ void zamet(ConsoleKeyInfo key)
         {
             Console.Clear();
             Console.SetCursorPosition(2, 1);
-            Console.WriteLine("1:Дата создания " + zametki[3].Dade);
+            Console.WriteLine("1:Дата " + zametki[3].Dade);
             Console.SetCursorPosition(2, 2);
             Console.WriteLine("2:Название " + zametki[3].Name);
             Console.SetCursorPosition(2, 3);
@@ -127,7 +142,7 @@ void zamet(ConsoleKeyInfo key)
         {
             Console.Clear();
             Console.SetCursorPosition(2, 1);
-            Console.WriteLine("1:Дата создания " + zametki[4].Dade);
+            Console.WriteLine("1:Дата " + zametki[4].Dade);
             Console.SetCursorPosition(2, 2);
             Console.WriteLine("2:Название " + zametki[4].Name);
             Console.SetCursorPosition(2, 3);
@@ -160,10 +175,12 @@ void Cursor(ConsoleKeyInfo key)
     }
     else if (key.Key == ConsoleKey.Escape)
     {
-        isListenning = false;
+        Bool = false;
     }
     Console.Clear();
 }
+
+
 
 void date2(ConsoleKeyInfo key)
 {
@@ -176,12 +193,15 @@ void date2(ConsoleKeyInfo key)
         plusDay--;
     }
 }
-while (isListenning == true)
+
+
+
+while (Bool == true)
 {
     ConsoleKeyInfo key = Console.ReadKey();
     Cursor(key);
     date2(key);
-    zamet(key);
+    zametka(key);
     Console.SetCursorPosition(0, 0);
     Console.WriteLine(now.AddDays(plusDay).ToLongDateString());
     Console.SetCursorPosition(0, posicion);
